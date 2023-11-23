0846119f4131797094f7bb5fe0c699f56b5c69eb
Markus Kaup <MarkusKaup@gmx.de>
2023-11-23 02:03:26 +0100

    Native_SQL

--- forum/src/app/components/subforum/subview/subview.component.ts
+++ forum/src/app/components/subforum/subview/subview.component.ts
@@ -6,18 +6,32 @@ import { SubforumService } from 'src/app/service/subforum.service';
 @Component({
   selector: 'app-subview',
   templateUrl: './subview.component.html',
-  styleUrls: ['./subview.component.css']
+  styleUrls: ['./subview.component.css'],
 })
 export class SubviewComponent implements OnInit {
-  threads!: Array<any>;
+  //threads: Array<ThreadModel> = [];
+  threads: Array<ThreadModel> = [];
+  thr!: ThreadModel;
 
-  constructor(private route: ActivatedRoute, private subService: SubforumService){}
+  constructor(
+    private route: ActivatedRoute,
+    private subService: SubforumService
+  ) {}
   ngOnInit(): void {
     const subName = this.route.snapshot.paramMap.get('subName')!;
-    this.subService.getAllThreadBySubforum(subName).subscribe(data =>
-      {this.threads = data;
-        console.log(this.threads);
+    this.subService.getAllThreadBySubforum(subName).subscribe((data) => {
+      for (let i = 0; i < data.length; i++) {
+        this.threads.push(new ThreadModel());
 
-      })
+        this.threads[i].threadId    = data[i][0];
+        this.threads[i].threadTitle = data[i][1];
+        this.threads[i].subforum    = data[i][2];
+        this.threads[i].user        = data[i][3];
+        this.threads[i].content     = data[i][4];
+        this.threads[i].date        = data[i][5];
+      }
+      //this.threads = data;
+      console.log(this.threads);
+    });
   }
 }
